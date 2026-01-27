a2af5d44b4eb8245fe94cfcb6a26ddf843f24ead
Locus Wong <wongtzswa@gmail.com>
2026-01-26 20:44:55 -0500

    Add source file

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Locus Wong on 2026-01-26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
