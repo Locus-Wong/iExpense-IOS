7512312c91b42620fa3b7bfb160b0f67606e6e5c
Locus Wong <wongtzswa@gmail.com>
2026-01-28 00:38:12 -0500

    Building a list we can delete from

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,37 +7,37 @@
 
 import SwiftUI
 
-@Observable
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
 }
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    var body: some View {
-        Button("Dismiss"){
-            dismiss()
-        }
-    }
+@Observable
+class Expense{
+    var items = [ExpenseItem]()
 }
 
 struct ContentView: View {
-    @State private var user = User()
-    @State private var showingSheet = false
-    var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            Button("Show Sheet"){
-                showingSheet.toggle()
+    @State private var expenses = Expense()
+    var body: some View{
+        NavigationStack{
+            List{
+                ForEach(expenses.items, id: \.name){item in Text(item.name)}
+                    .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar{
+                Button("Add Expense", systemImage: "plus"){
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
             }
         }
-        .sheet(isPresented: $showingSheet){
-           SecondView()
-        }
+    }
+
+    func removeItems(at offset: IndexSet){
+        expenses.items.remove(atOffsets: offset)
     }
 }
 
